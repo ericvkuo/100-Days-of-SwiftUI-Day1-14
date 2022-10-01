b90039fe7cc89a155a3b1d5ae80e0f16cf15bb52
Eric Kuo <111590422+ericvkuo@users.noreply.github.com>
2022-10-01 23:06:10 +0800

    change Day 9

---
+++ Day9-closures.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Cocoa
+
+let team = ["Gloria", "Suzanne", "Piper", "Tiffany", "Tasha"]
+let sortedTeam = team.sorted()
+print(sortedTeam)
+
+let captainFirstTeam = team.sorted(by: { (name1: String, name2: String) -> Bool in
+    if name1 == "Suzanne" {
+        return true
+    } else if name2 == "Suzanne" {
+        return false
+    }
+    return name1 < name2
+    
+})
+print(captainFirstTeam)
