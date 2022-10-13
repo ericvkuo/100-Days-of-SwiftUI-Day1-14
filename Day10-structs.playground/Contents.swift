ae71031db0655ff2d8e84f7df06b506c99fb3b32
Eric Kuo <111590422+ericvkuo@users.noreply.github.com>
2022-10-13 23:55:42 +0800

    finish Day 10

--- Day10-structs.playground/Contents.swift
+++ Day10-structs.playground/Contents.swift
@@ -69,3 +69,35 @@ print(rachel.vacationAllocated)
 
 rachel.vacationRemaning = 1
 print(rachel.vacationAllocated)
+print(rachel.vacationRemaning)
+
+struct Game {
+    var score = 0 {
+        didSet {
+            print("The score is now \(score)!")
+        }
+    }
+}
+
+var game = Game()
+game.score += 10
+
+game.score -= 3
+
+struct App {
+    var contacts = [String]() {
+        willSet {
+            print("Current value is \(contacts).")
+            print("New value will be \(newValue).")
+        }
+        
+        didSet {
+            print("There are now \(contacts.count) contacts.")
+            print("Old value was \(oldValue).")
+        }
+    }
+}
+
+var app = App()
+app.contacts.append("Adrian K")
+app.contacts.append("Laura J")
